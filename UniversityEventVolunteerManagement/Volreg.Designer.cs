ee36d0a8c3a05157fd55ed3f5b51dd6f1b57bf6b
Irtiza <Irtizayesmeen@gmail.com>
2026-01-20 00:02:04 +0600

    designed the registration form

--- UniversityEventVolunteerManagement/Volreg.Designer.cs
+++ UniversityEventVolunteerManagement/Volreg.Designer.cs
@@ -39,99 +39,114 @@
             btnRegister = new Button();
             label5 = new Label();
             lnklogin = new LinkLabel();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(194, 160);
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label1.Location = new Point(46, 95);
             label1.Name = "label1";
-            label1.Size = new Size(67, 15);
+            label1.Size = new Size(87, 19);
             label1.TabIndex = 0;
             label1.Text = "Full Name: ";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(219, 209);
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label2.Location = new Point(71, 149);
             label2.Name = "label2";
-            label2.Size = new Size(42, 15);
+            label2.Size = new Size(56, 19);
             label2.TabIndex = 1;
             label2.Text = "Email: ";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(167, 254);
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label3.Location = new Point(19, 194);
             label3.Name = "label3";
-            label3.Size = new Size(94, 15);
+            label3.Size = new Size(117, 19);
             label3.TabIndex = 2;
             label3.Text = "Phone Number: ";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(198, 306);
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label4.Location = new Point(50, 246);
             label4.Name = "label4";
-            label4.Size = new Size(63, 15);
+            label4.Size = new Size(81, 19);
             label4.TabIndex = 3;
             label4.Text = "Password: ";
             // 
             // txtFullName
             // 
-            txtFullName.Location = new Point(267, 152);
+            txtFullName.Location = new Point(142, 92);
             txtFullName.Name = "txtFullName";
-            txtFullName.Size = new Size(100, 23);
+            txtFullName.Size = new Size(144, 23);
             txtFullName.TabIndex = 4;
             // 
             // txtEmail
             // 
-            txtEmail.Location = new Point(267, 201);
+            txtEmail.Location = new Point(142, 141);
             txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(100, 23);
+            txtEmail.Size = new Size(144, 23);
             txtEmail.TabIndex = 5;
             // 
             // txtPhoneNumber
             // 
-            txtPhoneNumber.Location = new Point(267, 254);
+            txtPhoneNumber.Location = new Point(142, 194);
             txtPhoneNumber.Name = "txtPhoneNumber";
-            txtPhoneNumber.Size = new Size(100, 23);
+            txtPhoneNumber.Size = new Size(144, 23);
             txtPhoneNumber.TabIndex = 6;
             // 
             // txtPassword
             // 
             txtPassword.BackColor = SystemColors.ButtonHighlight;
-            txtPassword.Location = new Point(267, 298);
+            txtPassword.Location = new Point(142, 238);
             txtPassword.Name = "txtPassword";
             txtPassword.PasswordChar = '*';
             txtPassword.ReadOnly = true;
-            txtPassword.Size = new Size(100, 23);
+            txtPassword.Size = new Size(144, 23);
             txtPassword.TabIndex = 7;
             // 
             // btnRegister
             // 
-            btnRegister.Location = new Point(267, 362);
+            btnRegister.BackColor = Color.FromArgb(255, 255, 192);
+            btnRegister.Font = new Font("Times New Roman", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRegister.Location = new Point(122, 296);
             btnRegister.Name = "btnRegister";
-            btnRegister.Size = new Size(75, 23);
+            btnRegister.Size = new Size(107, 27);
             btnRegister.TabIndex = 8;
             btnRegister.Text = "Register";
-            btnRegister.UseVisualStyleBackColor = true;
+            btnRegister.UseVisualStyleBackColor = false;
             btnRegister.Click += btnRegister_Click;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(198, 63);
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(60, 17);
             label5.Name = "label5";
-            label5.Size = new Size(195, 30);
+            label5.Size = new Size(238, 31);
             label5.TabIndex = 9;
             label5.Text = "Registration Form ";
             // 
             // lnklogin
             // 
             lnklogin.AutoSize = true;
-            lnklogin.Location = new Point(210, 426);
+            lnklogin.BackColor = Color.White;
+            lnklogin.LinkColor = Color.Black;
+            lnklogin.Location = new Point(86, 364);
             lnklogin.Name = "lnklogin";
             lnklogin.Size = new Size(200, 15);
             lnklogin.TabIndex = 10;
@@ -139,26 +154,39 @@
             lnklogin.Text = "Already have an account? Click here!";
             lnklogin.LinkClicked += lnklogin_LinkClicked;
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(150, 235, 255, 255);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(lnklogin);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(txtFullName);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(txtEmail);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(btnRegister);
+            panel1.Controls.Add(txtPhoneNumber);
+            panel1.Controls.Add(txtPassword);
+            panel1.Location = new Point(151, 34);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(349, 391);
+            panel1.TabIndex = 11;
+            // 
             // Volreg
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            BackgroundImage = Properties.Resources._492072851_1151955406731062_4674252519964508692_n;
+            BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(652, 450);
-            Controls.Add(lnklogin);
-            Controls.Add(label5);
-            Controls.Add(btnRegister);
-            Controls.Add(txtPassword);
-            Controls.Add(txtPhoneNumber);
-            Controls.Add(txtEmail);
-            Controls.Add(txtFullName);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            Controls.Add(panel1);
             Name = "Volreg";
             Text = "Volreg";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -174,5 +202,6 @@
         private Button btnRegister;
         private Label label5;
         private LinkLabel lnklogin;
+        private Panel panel1;
     }
 }
